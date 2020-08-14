03013394a8b7cf2f6dcabacd77b8b0fb03713580
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2020-08-15 11:15:28 +1200

    header done

--- src/App.js
+++ src/App.js
@@ -4,11 +4,67 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Citizen Status Page
-        </p>
-      </header>
+      <div>
+        <div className="header__container">
+          <div className="header__left-part">
+            <div className="header__title">Civil Status</div>
+            <div className="header__location">Browns Bay, Auckland, NZ</div>
+          </div>
+          <div className="header__right-part">
+            <button className="fancy-button">Get Alerts</button>
+            <button className="fancy-button">Edit location</button>
+          </div>
+        </div>
+      </div>
+      <div className="overview">
+        <div className="overview__header">
+          <b>Browns Bay</b> is at Alert Level 3
+        </div>
+        <div className="overview__description">
+
+          <div className="line-item__container">
+            <div className="line-item__status-circle line-item__status-circle--red"></div>
+            <b>Travel</b> restrictions apply
+          </div>
+          <div className="line-item__container">
+            <div className="line-item__status-circle line-item__status-circle--red"></div>
+            <b>Water</b> restrictions apply
+          </div>
+
+          <div className="line-item__container">
+            <div className="line-item__status-circle line-item__status-circle--green"></div>
+            <b>Internet</b> available
+          </div>
+
+          <div className="line-item__container">
+            <div className="line-item__status-circle line-item__status-circle--yellow"></div>
+            <b>Power</b> issues
+          </div>
+
+          <div className="line-item__container">
+            <div className="line-item__status-circle line-item__status-circle--green"></div>
+            <b>Healthcare</b> available
+          </div>
+
+          <div className="line-item__container">
+            <div className="line-item__status-circle line-item__status-circle--green"></div>
+            <b>Security</b> no issues
+          </div>
+
+          <div className="line-item__container">
+            <div className="line-item__status-circle line-item__status-circle--green"></div>
+            <b>Waterware</b> no issues
+          </div>
+
+          <div className="line-item__container">
+            <div className="line-item__status-circle line-item__status-circle--green"></div>
+            <b>Essential goods</b> available
+          </div>
+
+        </div>
+      </div>
+
+
     </div>
   );
 }
