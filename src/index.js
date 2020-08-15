6c71b94ddac50596145a64171af24f017399c42b
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2020-08-15 21:46:02 +1200

    Added React Router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
+import AppDetails from "./AppDetails";
+import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={"/"}>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/details" component={AppDetails} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
