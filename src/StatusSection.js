a077b863ff344644641e02508a88cd2690e9bc0d
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2020-08-15 21:52:36 +1200

    Dynamic Routing

--- src/StatusSection.js
+++ src/StatusSection.js
@@ -1,14 +1,30 @@
 import React from "react";
-import Highlighter from './Highlighter'
+import Highlighter from "./Highlighter";
+import { useHistory } from "react-router-dom";
 
-const StatusSection = ({ category, icon, highlight, color, children }) => (
-  <div className="status-section">
-    <div className="status-section__header">
-      <div className={`status-section__icon status-section__icon--${color}`}><img src={`/${icon}`} alt={category} className="status-section__icon-img"/></div>
-      <div className="status-section__title"><Highlighter text={category} highlight={highlight}/></div>
+const StatusSection = ({ category, icon, highlight, color, children }) => {
+  let history = useHistory();
+
+  return (
+    <div
+      className="status-section"
+      onClick={() => history.push(`/details?category=${category}`)}
+    >
+      <div className="status-section__header">
+        <div className={`status-section__icon status-section__icon--${color}`}>
+          <img
+            src={`/${icon}`}
+            alt={category}
+            className="status-section__icon-img"
+          />
+        </div>
+        <div className="status-section__title">
+          <Highlighter text={category} highlight={highlight} />
+        </div>
+      </div>
+      <div className="status-section__content">{children}</div>
     </div>
-    <div className="status-section__content">{children}</div>
-  </div>
-);
+  );
+};
 
 export default StatusSection;
