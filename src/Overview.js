4dd559ff2ee67a4553f90831f3edbbf48d642db5
JacksonLaw <jackson@pocketful.app>
2020-08-15 23:16:22 +1200

    use the data in the frontend

--- src/Overview.js
+++ src/Overview.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Overview = ({ location, alertLevel, children }) => (
   <div className="overview">
-    <div className={`overview__header ${alertLevel !== 1 ? "overview__header--yellow" : ""}`}>
-      <b>{location}</b> is at Alert Level {alertLevel}
+    <div className={`overview__header ${alertLevel !== "Alert Level 1" ? "overview__header--yellow" : ""}`}>
+      <b>{location}</b> is at {alertLevel}
     </div>
     <div className="overview__description">{children}</div>
   </div>
