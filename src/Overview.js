94bbba3b1d3cccdc89dd004d56190dc452e2c0c4
Walter Lim <waltissomewhere@gmail.com>
2020-10-16 18:14:04 +1300

    shitty hax to fix alert levels

--- src/Overview.js
+++ src/Overview.js
@@ -3,7 +3,7 @@ import React from "react";
 const Overview = ({ location, alertLevel, children }) => (
   <div className="overview">
     <div className={`overview__header ${alertLevel !== "Alert Level 1" ? "overview__header--yellow" : ""}`}>
-      <b>{location}</b> is at {alertLevel}
+      <b>{location}</b> is at Alert Level 1
     </div>
     <div className="overview__description">{children}</div>
   </div>
